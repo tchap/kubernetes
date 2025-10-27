7a1ab1a17f30d86a7019d9aaa22d83f8a34a8bc8
Ondra Kupka <okupka@redhat.com>
2025-10-27 12:24:51 +0100

    controller/endpoint: Improve goroutine mgmt
    Make sure all threads are terminated when Run returns.

--- pkg/controller/endpoint/endpoints_controller.go
+++ pkg/controller/endpoint/endpoints_controller.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"sync"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
@@ -188,8 +189,13 @@ func (e *Controller) Run(ctx context.Context, workers int) {
 	e.eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: e.client.CoreV1().Events("")})
 	defer e.eventBroadcaster.Shutdown()
 
-	defer e.queue.ShutDown()
-	defer e.podQueue.ShutDown()
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		<-ctx.Done()
+		e.queue.ShutDown()
+		e.podQueue.ShutDown()
+	})
+	defer wg.Wait()
 
 	logger := klog.FromContext(ctx)
 	logger.Info("Starting endpoint controller")
@@ -200,16 +206,16 @@ func (e *Controller) Run(ctx context.Context, workers int) {
 	}
 
 	for i := 0; i < workers; i++ {
-		go wait.UntilWithContext(ctx, e.worker, e.workerLoopPeriod)
-		go wait.UntilWithContext(ctx, e.podWorker, e.workerLoopPeriod)
+		wg.Go(func() {
+			wait.UntilWithContext(ctx, e.worker, e.workerLoopPeriod)
+		})
+		wg.Go(func() {
+			wait.UntilWithContext(ctx, e.podWorker, e.workerLoopPeriod)
+		})
 	}
-
-	go func() {
-		defer utilruntime.HandleCrash()
+	wg.Go(func() {
 		e.checkLeftoverEndpoints()
-	}()
-
-	<-ctx.Done()
+	})
 }
 
 func podToEndpointAddressForService(svc *v1.Service, pod *v1.Pod) (*v1.EndpointAddress, error) {
